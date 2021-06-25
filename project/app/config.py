460e30e3d7c151d4773573b050bf3726838b5936
kdorling <kudorling@gmail.com>
2021-06-25 14:38:33 -0600

    Added postgres. Temporarily fixed issue where pylint would report AnyUrl as missing from pydantic in config.py.

--- project/app/config.py
+++ project/app/config.py
@@ -1,8 +1,10 @@
+# pylint: disable=no-name-in-module
+
 import logging
 import os
 from functools import lru_cache
 
-from pydantic import BaseSettings
+from pydantic import BaseSettings, AnyUrl
 
 
 log = logging.getLogger("uvicorn")
@@ -11,9 +13,10 @@ log = logging.getLogger("uvicorn")
 class Settings(BaseSettings):
     environment: str = os.getenv("ENVIRONMENT", "dev")
     testing: bool = os.getenv("TESTING", 0)
+    database_url: AnyUrl = os.environ.get("DATABASE_URL")
 
 
 @lru_cache()
 def get_settings() -> BaseSettings:
     log.info("Loading config settings from the environment...")
-    return Settings()
+    return Settings() 
