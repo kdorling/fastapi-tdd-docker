709ecb87b004deaf4229f2e9d6e6c03121f6d654
kdorling <kudorling@gmail.com>
2021-07-10 20:18:35 -0600

    Added update route.
    Updated existing routes to handle invalid URLS, invalid JSON, and invalid summary ids.

--- project/app/api/crud.py
+++ project/app/api/crud.py
@@ -1,7 +1,11 @@
 from typing import List, Union
 
-from app.models.pydantic import SummaryPayloadSchema
 from app.models.tortoise import TextSummary
+
+from app.models.pydantic import (  # isort:skip
+    SummaryPayloadSchema,
+    SummaryUpdatePayloadSchema,
+)
 
 
 async def post(payload: SummaryPayloadSchema) -> int:
@@ -25,3 +29,13 @@ async def get_all() -> List:
 async def delete(id: int) -> int:
     summary = await TextSummary.filter(id=id).first().delete()
     return summary
+
+
+async def put(id: int, payload: SummaryUpdatePayloadSchema) -> Union[dict, None]:
+    summary = await TextSummary.filter(id=id).update(
+        url=payload.url, summary=payload.summary
+    )
+    if summary:
+        updated_summary = await TextSummary.filter(id=id).first().values()
+        return updated_summary[0]
+    return None
